7e9f3300a844538cc3590a3cfa443c8e8e04b75e
Qiuweizhu <873457571@qq.com>
2025-12-03 21:15:14 +0800

    add test

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -42,6 +42,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("E:/cmake_planning_demo/build/src/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("E:/cmake_planning_demo/build/test/map_test/cmake_install.cmake")
+endif()
+
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
